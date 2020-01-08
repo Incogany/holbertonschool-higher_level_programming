fbb146f710e7173ad10a58200dc6582623f27225
Juan-Bogota <1213@holbertonschool.com>
2020-01-08 21:57:08 +0000

    [feature] Print Integer Error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,11 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
-        sys.stderr.write("Exception: Unknown format code 'd' for object of ")
-        sys.stderr.write("type 'str'\n")
-        return False
-    except TypeError:
-        sys.stderr.write("TypeError: non-empty format string passed to ")
-        sys.stderr.write("object.__format__\n")
+    except (ValueError, TypeError) as error:
+        sys.stderr.write("Exception: {}\n".format(error))
         return False
