79708269362ace45aadc0e0c0902f409aff795f7
Juan-Bogota <1213@holbertonschool.com>
2020-01-09 22:11:52 +0000

    [feature] Suare Getter and setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,6 +9,22 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
         """Method area return the square Area"""
         return(self.__size ** 2)
+
+    @property
+    def size(self):
+        """Method property of size and return the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Method size_setter atributte evaluate the value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
