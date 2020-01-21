6493d491746db351a906863e6522a3fc1a1674ea
Juan-Bogota <1213@holbertonschool.com>
2020-01-21 20:09:57 +0000

    [Feature] Load from Json

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+
+
+def load_from_json_file(filename):
+    """
+    function that writes an Object to a text file, using a JSON representation
+    """
+    with open(filename, 'r') as f:
+        h = json.loads(f.read())
+        return h
