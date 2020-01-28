91ceff5ff4584ed7633505dc3bbd933e68c60826
Juan-Bogota <1213@holbertonschool.com>
2020-01-27 19:46:52 -0500

    Revert "[Feature] Upload test JSON"
    This reverts commit e32859c0065616f385fd0d552e10d91d2319a4e5.

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -52,40 +52,5 @@ class Test_Base(unittest.TestCase):
         G1 = Base([1, 2])
         self.assertEqual(G1.id, [1, 2])
 
-    def test_validate7(self):
-        """Validate Dictionary to JSON"""
-        H1 = Rectangle(10, 7, 2, 8)
-        dictionary = H1.to_dictionary()
-        json_dictionary = Base.to_json_string([dictionary])
-        dicts = {'x': 2, 'width': 10, 'id': 4, 'height': 7, 'y': 8}
-        lists = '[{"x": 2, "y": 8, "id": 4, "height": 7, "width": 10}]'
-        self.assertEqual(dictionary, dicts)
-        self.assertEqual(json_dictionary, lists)
-
-    def test_validate8(self):
-        """JSON string to dictionary"""
-        list_input = [
-            {'id': 89, 'width': 10, 'height': 4},
-            {'id': 7, 'width': 1, 'height': 7}
-        ]
-        json_list_input = Rectangle.to_json_string(list_input)
-        list_output = Rectangle.from_json_string(json_list_input)
-        l = [
-            {'height': 4, 'width': 10, 'id': 89},
-            {'height': 7, 'width': 1, 'id': 7}
-        ]
-        self.assertEqual(list_output, l)
-        self.assertEqual(isinstance(list_output, list), True)
-
-    def test_validate9(self):
-        """Dictionary to instance"""
-        I1 = Rectangle(3, 5, 1)
-        I1_dictionary = I1.to_dictionary()
-        I2 = Rectangle.create(**I1_dictionary)
-        self.assertEqual(I2.__str__(), "[Rectangle] (5) 1/0 - 3/5")
-        self.assertNotEqual(I1, I2)
-        self.assertIsNot(I1, I2)
-
-
 if __name__ == "__main__":
     unittest.main()
