d5aa66aa5aa63d2efde31b43175390d0d3110820
Juan-Bogota <1213@holbertonschool.com>
2020-01-28 20:58:23 +0000

    [feature] Advance Task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -81,3 +81,33 @@ class Base:
                 return create
         except IOError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        save to file CSV
+        """
+        lists = []
+        if len(list_objs) is not 0:
+            for i in list_objs:
+                lists.append(i.to_dictionary())
+        dicts = cls.to_json_string(lists)
+
+        with open(cls.__name__ + ".csv", "w+") as my_file:
+            my_file.write(dicts)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        returns a list of instances
+        """
+        try:
+            with open(cls.__name__ + ".csv", "r") as my_file:
+                read = my_file.read()
+                lists = Base.from_json_string(read)
+                create = []
+                for i in lists:
+                    create.append(cls.create(**i))
+                return create
+        except IOError:
+            return []
