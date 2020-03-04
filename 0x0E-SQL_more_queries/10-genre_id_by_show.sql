ae1e57ee9cd6af90bb57d1fcdb35d813fe49860a
Juan-Bogota <1213@holbertonschool.com>
2020-03-04 02:09:07 +0000

    [feature] Genre Id by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- script that lists all cities contained in the database hbtn_0d_usa.
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id ORDER BY tv_shows.title ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
