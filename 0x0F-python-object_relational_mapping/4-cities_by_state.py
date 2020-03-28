b8f39d410e53a168a71951f618463f69a5fa682a
Juan-Bogota <1213@holbertonschool.com>
2020-03-28 18:00:14 -0500

    [Feature] Cities By state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument.
-Write one that is safe from MySQL injections!
+script that lists all cities from the database hbtn_0e_4_usa
 """
 
 if __name__ == "__main__":
