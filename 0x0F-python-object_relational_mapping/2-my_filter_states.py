dfaf6f16e7e50f1c94a53edb601a01d56c225ead
Juan-Bogota <1213@holbertonschool.com>
2020-03-28 16:14:58 -0500

    [Feature] Filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,10 +7,11 @@ states table of hbtn_0e_0_usa where name matches the argument.
 if __name__ == "__main__":
     import MySQLdb
     import sys
+    arg = sys.argv[4]
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name ='{}' ORDER BY id".format(sys.argv[4]))
+    c.execute("SELECT * FROM states WHERE name ='{}' ORDER BY id".format(arg))
     my_states = c.fetchall()
     for state in my_states:
         if sys.argv[4] == state[1]:
