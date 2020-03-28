3a15a100ba9e99e16a52c3f3912d3fe713c9be7f
Juan-Bogota <1213@holbertonschool.com>
2020-03-28 16:13:31 -0500

    [Feature] Filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY id")
+    c.execute("SELECT * FROM states WHERE name ='{}' ORDER BY id".format(sys.argv[4]))
     my_states = c.fetchall()
     for state in my_states:
         if sys.argv[4] == state[1]:
