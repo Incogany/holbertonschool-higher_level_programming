bb069d0232150024eff6362c927b4268c90d780d
Juan-Bogota <1213@holbertonschool.com>
2020-03-29 13:19:58 -0500

    [Feature] Fist State SQLAlchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
     session = Session()
-    for state in session.query(State).order_by(State.id):
-        if state.id is 1:
-            print("{}: {}".format(state.id, state.name))
+    for state in session.query(State).order_by(State.id)[:1]:
+        print("{}: {}".format(state.id, state.name))
     session.close()
