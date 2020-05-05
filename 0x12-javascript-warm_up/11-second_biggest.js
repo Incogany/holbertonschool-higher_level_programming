dea50e09ae60ed27353309dc53aee22604bc23a8
Juan Lopez <1213@holbertonschool.com>
2020-05-05 21:29:36 +0000

    [Feature] Correction file Output wrong

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,5 +2,9 @@
 // script that prints add two numbers
 const myList = process.argv;
 myList.splice(0, 2);
-myList.sort((a, b) => a - b);
-console.log(myList[myList.length - 2]);
+if (myList.length < 2) {
+  console.log('0');
+} else {
+  myList.sort((a, b) => a - b);
+  console.log(myList[myList.length - 2]);
+}
