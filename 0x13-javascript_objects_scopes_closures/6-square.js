018ac6224994ad12d76aed4a5bf47d6b337b6db4
Juan-Bogota <1213@holbertonschool.com>
2020-05-11 21:58:40 -0500

    [Feature] Square Print

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,8 +8,8 @@ const SquareR = require('./5-square');
 class Square extends SquareR {
   charPrint (c) {
     if (c === undefined) { c = 'X'; }
-    for (let x = 0; x < this.size; x++) {
-      console.log(c.repeat(this.size));
+    for (let x = 0; x < this.height; x++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
