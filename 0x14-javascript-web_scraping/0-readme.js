9db569923efd7bae5d6a465878c2c9a71cfaa0b4
Juan-Bogota <1213@holbertonschool.com>
2020-05-12 13:57:00 -0500

    [Feature] Readme File and JS file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file.
+
+const fs = require('fs');
+try {
+  const data = fs.readFileSync(process.argv[2], 'utf8');
+  console.log(data);
+} catch (err) {
+  console.log(err);
+}
